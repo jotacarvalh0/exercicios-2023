eafac168ac72048db2ccf7edfe3170020dbe00a8
jotacarvalh0 <josecarvalho11235@gmail.com>
2023-10-07 14:51:15 -0300

    FEAT: SideBar criada e estilizada

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from './components/sidebar/sidebar.component'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'DevChuva';
+}
